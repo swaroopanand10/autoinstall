9ea6be1faaf104583a91e7e710fae47500f9e0b8
swaroopanand <swaroopanand695@gmail.com>
2023-11-18 21:51:12 +0530

    dded code for changing persmission for keys into ansible file and removed it from repos file

--- ansible-ps.sh
+++ ansible-ps.sh
@@ -7,6 +7,8 @@ mkdir -p ~/.ssh/
 ansible-vault view --vault-password-file ~/.vault-key ./ans/ansibler.txt > ~/.ssh/id_ed25519
 ansible-vault view --vault-password-file ~/.vault-key ./ans/ansibleu.txt > ~/.ssh/id_ed25519.pub
 
+chmod 600 $HOME/.ssh/*
+
 #just for testing
 # ansible-vault view --vault-password-file ~/.vault-key ./ans/ansibler.txt > ~/ssh/id_ed25519
 # ansible-vault view --vault-password-file ~/.vault-key ./ans/ansibleu.txt > ~/ssh/id_ed25519.pub
