db07ca05bb38d6cf952a3ef2c5c01b3ecd636dae
swaroopanand <swaroopanand@email.com>
2024-03-12 21:52:28 +0530

    wrote another regex to check already installed package just for sake of learning, working for now ok

--- paru-function.sh
+++ paru-function.sh
@@ -2,7 +2,8 @@
 
 _isInstalledParu() {
     package="$1";
-    check="$(paru -Qs "${package}" | grep "local" | grep "local/${package} ")";
+    # check="$(paru -Qs "${package}" | grep "local" | grep "local/${package} ")"; # This is working perfectly, just wrote a new one for experiment
+    check="$(paru -Qs "${package}" | grep "local/${package}\s")"; # a space is neccesary at the end so that, ex- searching for "neovim" does not match to "neovim-nightly" as the space is after the full package name.
     if [ -n "${check}" ] ; then
         echo 0; #'0' means 'true' in Bash
         return; #true
